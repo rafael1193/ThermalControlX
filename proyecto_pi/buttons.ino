2f4f9f6c334947cf57fd3568a1a795213c05fb25
Rafael Bailón <rafaelbailon@ieee.org>
2014-02-08 12:25:17 +0100

    Increased debounce delay

--- proyecto_pi/buttons.ino
+++ proyecto_pi/buttons.ino
@@ -47,7 +47,7 @@ int lastButtonState = LOW;   // the previous reading from the input pin
  // the following variables are long's because the time, measured in miliseconds,
  // will quickly become a bigger number than can be stored in an int.
 long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 200;    // the debounce time; increase if the output flickers
+long debounceDelay = 500;    // the debounce time; increase if the output flickers
 
 void buttons_init() {
   
